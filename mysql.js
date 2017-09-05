22a018db330ad054f89cf8707f59ef274aa003fc
ToxicOC <31538235+ToxicOC@users.noreply.github.com>
2017-09-05 14:29:34 +0200

    mysql connect

---
+++ mysql.js
@@ -0,0 +1,15 @@
+/// datei für mysql stuff für connection un querys
+
+var mysql = require('mysql');     // include für mysql node modul
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword",
+  database: "mydb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
